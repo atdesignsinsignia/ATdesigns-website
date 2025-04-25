34a2050b6b2207b70d19cb4b17a95fa7495f771a
Daniel Belleza <bellezabrndn@gmail.com>
2025-04-25 08:47:49 -0400

    updated catalogs

--- js/geoRedirect.js
+++ js/geoRedirect.js
@@ -6,10 +6,10 @@ function redirectToCatalog() {
             const country = data.country_code;
             if (country === "US") {
                 // Redirect to US catalog
-                window.location.href = "https://sageflip.com/50176-2023/";
+                window.location.href = "https://sageflip.com/50176-2025/";
             } else if (country === "CA") {
                 // Redirect to Canada catalog
-                window.location.href = "https://sageflip.com/67430-2023/";
+                window.location.href = "https://sageflip.com/67430-2025/";
             } else {
                 // Optional: Handle visitors from other countries
                 alert("Catalog is not available for your location.");
